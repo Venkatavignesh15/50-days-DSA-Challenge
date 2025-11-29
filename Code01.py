1f830e6bf3d14b54c9a6c23ebef6461e8db6550b
Venkata Vignesh <venkatavignesh7@gmail.com>
2025-11-29 20:11:12 +0530

    Update Day-01
    # Day 1 – Count Elements Greater Than Previous Average
    
    Problem from: HackerRank  
    Difficulty: Easy  
    
    ## What I Learned
    - Running average concept
    - Using lists efficiently
    - Comparing current value with dynamic previous average

--- Code01.py
+++ Code01.py
@@ -1,22 +1,18 @@
 def countResponseTimeRegressions(responseTimes):
-    avg_list = []
-    count = 0
-
-    for i in range(1, len(responseTimes)):
-        prev_avg = sum(avg_list) / len(avg_list) if avg_list else responseTimes[0]
-
-        if responseTimes[i] > prev_avg:
-            count += 1
+    avg=()
+    count=0
+    for i in range(1,len(responseTimes)):
+        pre_avg=sum(avg)/len(avg)
+        if responseTimes[i]>pre_avg:
+            count+=1
+            avg.append(responseTimes[i])# Write your code here
         else:
-            count = 1
-
-        avg_list.append(responseTimes[i])
-
+            count=1
+            avg.append(responseTimes[i])
     return count
-
-
 if __name__ == '__main__':
     responseTimes_count = int(input().strip())
+
     responseTimes = []
 
     for _ in range(responseTimes_count):
@@ -24,4 +20,5 @@ if __name__ == '__main__':
         responseTimes.append(responseTimes_item)
 
     result = countResponseTimeRegressions(responseTimes)
+
     print(result)

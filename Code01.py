7387977fbd3b07def10dec378eefe3a22a583de3
Venkata Vignesh <venkatavignesh7@gmail.com>
2025-11-30 11:03:04 +0530

    Update Code01.py

--- Code01.py
+++ Code01.py
@@ -1,15 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'countResponseTimeRegressions' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY responseTimes as parameter.
+#
+
 def countResponseTimeRegressions(responseTimes):
-    avg=()
+    if not responseTimes:
+        return 0
+    lst=[responseTimes[0]]
     count=0
+    if len(responseTimes)<=1:
+        return count
     for i in range(1,len(responseTimes)):
-        pre_avg=sum(avg)/len(avg)
+        pre_avg=sum(lst)/len(lst)
         if responseTimes[i]>pre_avg:
-            count+=1
-            avg.append(responseTimes[i])# Write your code here
-        else:
-            count=1
-            avg.append(responseTimes[i])
+                count+=1
+        lst.append(responseTimes[i])
     return count
+
 if __name__ == '__main__':
     responseTimes_count = int(input().strip())
 

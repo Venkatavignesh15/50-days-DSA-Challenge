23ae57064015378eb63270aeaec02b9dba554ca5
Venkata Vignesh <venkatavignesh7@gmail.com>
2025-12-30 16:19:00 +0530

    Two Sum
    🔢 Problem: Two Sum
    
    Difficulty: Easy → Medium
    Concepts: Arrays | Hash Map | Lookup Optimization
    
    🧠 Problem Statement
    
    Given an array of positive integers and a target integer, return the indices of two elements whose sum equals the target.
    If no such pair exists, return [-1, -1].
    
    ✅ Optimized Approach (O(n) Time)
    
    Use a hash map to store previously seen numbers and their indices
    
    For each element, calculate the complement needed to reach the target
    
    If the complement exists, return both indices
    
    Otherwise, store the current value and continue
    
    🧪 Example
    
    Input:
    nums = [2, 7, 11, 15]
    target = 9
    
    Output:
    [0, 1]
    👉 Because 2 + 7 = 9

---
+++ Code29.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'findTaskPairForSlot' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY taskDurations
+#  2. INTEGER slotLength
+#
+
+def findTaskPairForSlot(taskDurations, slotLength):
+    # Write your code here
+    seen={}
+    if len(taskDurations)<2:
+        return [-1,-1]
+    for i,j in enumerate(taskDurations):
+        val=slotLength-j
+        if val in seen:
+            return [seen[val],i]
+        seen[j]=i
+    return [-1,-1]
+
+if __name__ == '__main__':
+    taskDurations_count = int(input().strip())
+
+    taskDurations = []
+
+    for _ in range(taskDurations_count):
+        taskDurations_item = int(input().strip())
+        taskDurations.append(taskDurations_item)
+
+    slotLength = int(input().strip())
+
+    result = findTaskPairForSlot(taskDurations, slotLength)
+
+    print('\n'.join(map(str, result)))

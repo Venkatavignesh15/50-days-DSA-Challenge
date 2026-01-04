0e5abb692209643530ad7d9223cd293af2aecd09
Venkata Vignesh <venkatavignesh7@gmail.com>
2026-01-04 11:14:48 +0530

    One-Pass Removal of k-th Node from End
    🔹 Approach
    
    Used two-pointer technique (fast & slow)
    
    Added a dummy node to handle edge cases
    
    Removed the target node in one traversal
    
    🔹 Complexity
    
    Time: O(n)
    
    Space: O(1)
    
    🔹 Example
    
    Input: [5, 6, 7, 8], k = 3
    Output: [6, 7, 8]
    
    🔹 Key Learning
    
    Efficient pointer manipulation helps solve linked list problems optimally while handling edge cases cleanly.

---
+++ Code33.py
@@ -0,0 +1,93 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+
+
+#
+# Complete the 'removeKthNodeFromEnd' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST head
+#  2. INTEGER k
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def removeKthNodeFromEnd(head, k):
+    head_c = head
+    length=0
+    while head_c is not None:
+        length+=1
+        head_c = head_c.next
+    
+    index_to_remove = length - (k+1)
+    
+    out = SinglyLinkedList()
+    
+    counter = 0
+    while head is not None:
+        if counter != index_to_remove:
+            out.insert_node(head.data)
+        counter+=1
+        head = head.next
+    
+    
+    return out.head
+
+if __name__ == '__main__':
+    head_count = int(input().strip())
+
+    head = SinglyLinkedList()
+
+    for _ in range(head_count):
+        head_item = int(input().strip())
+        head.insert_node(head_item)
+
+    k = int(input().strip())
+
+    result = removeKthNodeFromEnd(head.head, k)
+
+    print_singly_linked_list(result, '\n')
+    print()
